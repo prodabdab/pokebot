3a197fe97cf7fbbd9b40b45f8bf59bd6fbc440ec
prodabdab <73546028+prodabdab@users.noreply.github.com>
2021-06-09 11:14:36 +0530

    Update my_config_example.js

--- config/my_config_example.js
+++ config/my_config_example.js
@@ -5,20 +5,20 @@ var myconfig = {
     "database": {
         host: "localhost",
         user: "root",
-        password: "~~~password_for_root~~~",
-        database: '~~~database_name~~~',
+        password: "Yatin1NOV",
+        database: 'POKEZ',
         charset: 'utf8mb4'
     },
 
     /**
      * Discord bot token
      */
-    "token": '~~~token_here~~~',
+    "token": 'ODMxODk4NzUzNzg2NDQ1ODc0.YHb8Fg.6dJLQUp6_630R6fD6FbQ7NxC110',
 
     /**
      * true = Pokebot will attach local images from this repository on your computer when possible for messages
      * false = Pokebot will always use image URLs from the online repository for messages
      */
-    "useLocalImages": true
+    "useLocalImages": false,
 };
 module.exports = myconfig;
